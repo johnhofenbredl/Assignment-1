739a6646d9ff90460a40860bdc04a4d4fe4407fd
johnhofenbredl <hofenbrj@oregonstate.edu>
2024-06-25 19:34:43 -0700

    Add files via upload

---
+++ animal.py
@@ -0,0 +1,6 @@
+#Author: John Hofenbredl
+#GitHub username: hofenbrj@oregonstate.edu
+#Date: 06/23/2024
+#Description: Asks the user for their favorite animal and then prints out that that is their favorite animal
+fave_animal= input("Please enter your favorite animal:")
+print("Your favorite animal is the "+fave_animal+".")
